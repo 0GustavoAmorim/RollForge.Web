814fbb3ae745a3bda4af86fbd8dc7d7a9926e2ed
Gustavo Amorim <gustavoamorilsilva@gmail.com>
2025-10-14 17:11:07 -0300

    criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-400">🎲 RollForge</h1>
+      <p className="text-gray-400 mt-2">Frontend em React + SignalR</p>
+    </div>
+  );
+}
